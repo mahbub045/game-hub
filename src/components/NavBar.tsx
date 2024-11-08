cd1eb40b258d590a7053f2dbcb6839c32eeed870
Md Mahbub Rahman <mahbubrmn45@gmail.com>
2024-11-08 18:29:00 +0600

    8- Building the Color Mode Switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ import { ColorModeButton } from "./ui/color-mode";
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent="space-between" padding="10px">
       <Image src={logo} boxSize="60px" />
       <Text>NavBar</Text>
       <ColorModeButton />
