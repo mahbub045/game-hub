a116dd34f88980df0ba3148bae643a755515f911
Md Mahbub Rahman <mahbubrmn45@gmail.com>
2024-11-09 21:51:07 +0600

    fixed layout

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card.Root height="300px">
+    <Card.Root>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="xl">{game.name}</Heading>
