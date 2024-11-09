903abb3631f6ff478cfaa484eee4eae5bbc1dc54
Md Mahbub Rahman <mahbubrmn45@gmail.com>
2024-11-09 11:53:04 +0600

    12- Displaying Platform Icons

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,60 +1,44 @@
 import { Platfrom } from "@/hooks/useGames";
-import { Text } from "@chakra-ui/react";
+import { HStack, Text } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+import { BsGlobe } from "react-icons/bs";
+import {
+  FaAndroid,
+  FaApple,
+  FaLinux,
+  FaPlaystation,
+  FaWindows,
+  FaXbox,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
 interface Props {
   platforms: Platfrom[];
 }
 
 const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    android: FaAndroid,
+    web: BsGlobe,
+  };
   return (
-    <>
+    <HStack marginY={"10px"}>
       {platforms.map((platform) => (
-        <Text>{platform.name}</Text>
+        <Text
+          key={platform.id} // Ensure each Icon has a unique key
+          as={iconMap[platform.slug]} // Use `platform.slug` for mapping
+          color="gray.500"
+        />
       ))}
-    </>
+    </HStack>
   );
 };
 
 export default PlatformIconList;
-
-
-
-// import { Platfrom } from "@/hooks/useGames";
-// import { HStack, Icon } from "@chakra-ui/react";
-// import { IconType } from "react-icons";
-// import { BsGlobe } from "react-icons/bs";
-// import {
-//   FaAndroid,
-//   FaApple,
-//   FaLinux,
-//   FaPlaystation,
-//   FaWindows,
-//   FaXbox,
-// } from "react-icons/fa";
-// import { MdPhoneIphone } from "react-icons/md";
-// import { SiNintendo } from "react-icons/si";
-// interface Props {
-//   platforms: Platfrom[];
-// }
-
-// const PlatformIconList = ({ platforms }: Props) => {
-//   const iconMap: { [key: string]: IconType } = {
-//     pc: FaWindows,
-//     playstation: FaPlaystation,
-//     xbox: FaXbox,
-//     nintendo: SiNintendo,
-//     mac: FaApple,
-//     linux: FaLinux,
-//     ios: MdPhoneIphone,
-//     android: FaAndroid,
-//     web: BsGlobe,
-//   };
-//   return (
-//     <HStack marginY={"10px"}>
-//       {platforms.map((platform) => (
-//         <Icon as={iconMap[platform.slug]} color="gray.500" />
-//       ))}
-//     </HStack>
-//   );
-// };
-
-// export default PlatformIconList;
