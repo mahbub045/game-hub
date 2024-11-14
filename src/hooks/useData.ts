90b2b00388775e395314d5c3292f58eac47db58a
Md Mahbub Rahman <mahbubrmn45@gmail.com>
2024-11-14 21:41:08 +0600

    0031_26_Exercise_Fetching_Games

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,46 +1,5 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import apiClient from "@/services/api-client";
-import { AxiosRequestConfig, CanceledError } from "axios";
-import { useEffect, useState } from "react";
+ 
 
-export interface FetchResponse<T> {
-  count: number;
-  results: T[];
-}
 
-const useData = <T>(
-  endpoint: string,
-  requestConfig?: AxiosRequestConfig,
-  deps: unknown[] = [] // Use unknown[] to avoid any type warning
-) => {
-  const [data, setData] = useState<T[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    const controller = new AbortController();
 
-    setIsLoading(true);
-    apiClient
-      .get<FetchResponse<T>>(endpoint, {
-        signal: controller.signal,
-        ...requestConfig,
-      })
-      .then((res) => {
-        setData(res.data.results);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        if (!(err instanceof CanceledError)) {
-          setError(err.message);
-          setIsLoading(false); // Update isLoading to false if there's an error
-        }
-      });
-
-    return () => controller.abort();
-  }, deps);
-
-  return { data, error, isLoading };
-};
-
-export default useData;
