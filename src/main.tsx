bf40014c84f5ff0a2c08dbbc81d54b6eee1c65d8
Md Mahbub Rahman <mahbubrmn45@gmail.com>
2024-11-15 20:36:35 +0600

    Sentup routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import { RouterProvider } from "react-router-dom";
 import { ColorModeProvider } from "./components/ui/color-mode";
 import "./index.css";
+import router from "./routes";
 
 const queryClient = new QueryClient();
 
@@ -14,7 +15,7 @@ createRoot(document.getElementById("root")!).render(
     <ChakraProvider value={defaultSystem}>
       <ColorModeProvider>
         <QueryClientProvider client={queryClient}>
-          <App />
+          <RouterProvider router={router} />
         </QueryClientProvider>
       </ColorModeProvider>
     </ChakraProvider>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,28 +1,22 @@
-import { Box, Flex, Grid, GridItem, Stack } from "@chakra-ui/react";
-import "./App.css";
-import GameGrid from "./components/GameGrid";
-import GameHeading from "./components/GameHeading";
-import GenreList from "./components/GenreList";
-import NavBar from "./components/NavBar";
-import PlatformSelector from "./components/PlatformSelector";
-import SortSelector from "./components/SortSelector";
+import GameGrid from "@/components/GameGrid";
+import GameHeading from "@/components/GameHeading";
+import GenreList from "@/components/GenreList";
+import PlatformSelector from "@/components/PlatformSelector";
+import SortSelector from "@/components/SortSelector";
+import { Grid, Stack, GridItem, Box, Flex } from "@chakra-ui/react";
 
-function App() {
+const HomePage = () => {
   return (
     <Grid
       templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
+        base: `"main"`,
+        lg: `"aside main"`,
       }}
       templateColumns={{
         base: "1fr",
         lg: "200px 1fr",
       }}
     >
-      {/* Nave Bar */}
-      <GridItem area="nav">
-        <NavBar />
-      </GridItem>
       {/* Side Bar */}
       <Stack hideBelow="lg">
         <GridItem area="aside" paddingX={5}>
@@ -46,6 +40,6 @@ function App() {
       </GridItem>
     </Grid>
   );
-}
+};
 
-export default App;
+export default HomePage;
