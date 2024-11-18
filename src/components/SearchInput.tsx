8f3f535df5e775f72e59094091f0b9fd7c6b34be
Md Mahbub Rahman <mahbubrmn45@gmail.com>
2024-11-18 19:38:01 +0600

    Fixed the NavBar

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,16 +1,21 @@
 import useGameQueryStore from "@/store";
 import { Flex, Input } from "@chakra-ui/react";
 import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
   const ref = useRef<HTMLInputElement>(null);
   const setSearchText = useGameQueryStore((s) => s.setSearchText);
+  const navigate = useNavigate();
 
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
-        if (ref.current) setSearchText(ref.current.value);
+        if (ref.current) {
+          setSearchText(ref.current.value);
+          navigate("/");
+        }
       }}
     >
       <Flex
