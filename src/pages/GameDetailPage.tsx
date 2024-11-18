70ffef544d19b37f8b12cfdd17af99e2a11ea97f
Md Mahbub Rahman <mahbubrmn45@gmail.com>
2024-11-18 15:41:28 +0600

    Build Expoandable Text

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,16 +1,17 @@
+import ExpandableText from "@/components/ExpandableText";
 import useGame from "@/hooks/useGame";
-import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 
 const GameDetailPage = () => {
-  const { slug } = useParams();
+  const { slug } = useParams<{ slug: string }>();
   const { data: game, isLoading, error } = useGame(slug!);
   if (isLoading) return <Spinner />;
   if (error || !game) return error;
   return (
     <>
       <Heading>{game.name}</Heading>
-      <Text>{game.description_raw}</Text>
+      <ExpandableText>{game.description_raw}</ExpandableText>
     </>
   );
 };
