aebff63fca23f9f806fb93b11d55a57bcbb1df80
Md Mahbub Rahman <mahbubrmn45@gmail.com>
2024-11-18 19:28:13 +0600

    Update Meta score

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -5,10 +5,15 @@ interface Props {
 }
 
 const CriticScore = ({ score }: Props) => {
-  const color = score > 85 ? "green" : score > 60 ? "yellow" : "";
-  return (
+  const color = score > 85 ? "green" : score > 60 ? "yellow" : "red"; // Default to "red" for low scores
+
+  return score ? (
     <Badge color={color} fontSize="14px" paddingX={2} borderRadius="4px">
       {score}
+    </Badge>
+  ) : (
+    <Badge color="red" fontSize="14px" paddingX={2} borderRadius="4px">
+      None
     </Badge>
   );
 };
